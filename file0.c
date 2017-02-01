4430760511546174470431804f3bd2494e31bf0d
issi7915 <isaiahbaron@gmail.com>
2017-02-01 16:15:33 -0700

    Add and subtract

---
+++ file0.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+
+int main(){
+	int a = 12;
+	int b = 5
+	int c = 4;
+	
+	add(a,b,c);
+}
+
+int add(int a, int b, int c){
+	return a+b+c;
+}
---
+++ file1.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+
+int main(){
+	int a = 45;
+	int b = 41;
+
+	sub(a,b);
+
+}
+
+int sub(int a, int b){
+	return a - b;
+}
